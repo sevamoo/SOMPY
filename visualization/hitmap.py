453f4db71e598887197099c4cd311e03e3ee93f5
Sebastian <superpacko@gmail.com>
2016-01-30 19:56:34 -0300

    added visualization modules: dotmap, histogram, hitmap, mapview and umatrix

---
+++ visualization/hitmap.py
@@ -0,0 +1,33 @@
+import matplotlib
+from . import MatplotView
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+class HitMapView(MatplotView):
+
+    def _set_labels(self, cents, ax, labels):
+        for i, txt in enumerate(labels):
+            ax.annotate(txt, (cents[i, 1], cents[i, 0]), size=10, va="center")
+
+    def show(self, som, data=None):
+        codebook = som.cluster_labels if hasattr(som, 'cluster_labels') else som.cluster()
+        msz = som.codebook.mapsize
+
+        self.prepare()
+        ax = self._fig.add_subplot(111)
+
+        if data:
+            proj = som.project_data(data)
+            cents = som.bmu_ind_to_xy(proj)
+            self._set_labels(cents, ax, codebook[proj])
+
+        else:
+            cents = som.bmu_ind_to_xy(np.arange(0, msz[0]*msz[1]))
+            self._set_labels(cents, ax, codebook)
+
+        plt.imshow(codebook.reshape(msz[0], msz[1])[::], alpha=.5)
+        plt.show()
+
+        return cents
+
--- visualization/mapview.py
+++ visualization/mapview.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 class MapView(MatplotView):
 
-    def _setup_figure(self, som, which_dim):
+    def _calculate_figure_params(self, som, which_dim):
         codebook = som._normalizer.denormalize_by(som.data_raw, som.codebook.matrix)
         dim = som._dim
         indtoshow, sV, sH = None, None, None
@@ -17,7 +17,7 @@ class MapView(MatplotView):
             indtoshow = np.arange(0, dim).T
             ratio = float(dim)/float(dim)
             ratio = np.max((.35, ratio))
-            sH, sV = 16,16*ratio*1
+            sH, sV = 16, 16*ratio*1
 
         elif type(which_dim) == int:
             dim = 1
@@ -33,21 +33,21 @@ class MapView(MatplotView):
             indtoshow = np.asarray(which_dim).T
             sH, sV = 16, 16*ratio*1
 
-        self.width = sH
-        self.height = sV
-        self.prepare()
-
         no_row_in_plot = dim/6 + 1  # 6 is arbitrarily selected
         no_col_in_plot = dim if no_row_in_plot <= 1 else 6
         axisNum = 0
 
-        return indtoshow, no_row_in_plot, no_col_in_plot, axisNum
+        width = sH
+        height = sV
+
+        return width, height, indtoshow, no_row_in_plot, no_col_in_plot, axisNum
 
 
 class View2D(MapView):
 
     def show(self, som, which_dim='all'):
-        indtoshow, no_row_in_plot, no_col_in_plot, axisNum = self._setup_figure(som, which_dim)
+        self.width, self.height, indtoshow, no_row_in_plot, no_col_in_plot, axisNum = self._calculate_figure_params(som, which_dim)
+        self.prepare()
         codebook = som.codebook.matrix
 
         norm = matplotlib.colors.normalize(vmin=np.mean(codebook.flatten())-1*np.std(codebook.flatten()),
@@ -59,10 +59,6 @@ class View2D(MapView):
             ind = int(indtoshow[axisNum-1])
             mp = codebook[:, ind].reshape(som.codebook.mapsize[0], som.codebook.mapsize[1])
             pl = plt.pcolor(mp[::-1], norm=norm)
-            plt.title(som.component_names[0][ind])
-            font = {'size': self.text_size*self.height/no_col_in_plot}
-            plt.rc('font', **font)
-            plt.axis('off')
             plt.axis([0, som.codebook.mapsize[0], 0, som.codebook.mapsize[1]])
             ax.set_yticklabels([])
             ax.set_xticklabels([])
@@ -73,92 +69,65 @@ class View2D(MapView):
 
 class View2DPacked(MapView):
 
-    def view_2d_Pack(self, som, what='codebook', which_dim='all', grid='yes', text='yes', CMAP=None, col_sz=None):
-        indtoshow, no_row_in_plot, no_col_in_plot, axisNum = self._setup_figure(som, which_dim)
+    def _set_axis(self, ax, msz0, msz1):
+        plt.axis([0, msz0, 0, msz1])
+        ax.set_yticklabels([])
+        ax.set_xticklabels([])
+        ax.xaxis.set_ticks([i for i in range(0, msz1)])
+        ax.yaxis.set_ticks([i for i in range(0, msz0)])
+        ax.xaxis.set_ticklabels([])
+        ax.yaxis.set_ticklabels([])
+        ax.grid(True, linestyle='-', linewidth=0.5, color='k')
+
+    def show(self, som, what='codebook', which_dim='all', CMAP=None, col_sz=None):
+        self.width, self.height, indtoshow, no_row_in_plot, no_col_in_plot, axisNum = self._calculate_figure_params(som, which_dim)
         codebook = som.codebook.matrix
 
-        no_col_in_plot = som._dim if no_row_in_plot <= 1 else col_sz
+        no_col_in_plot = som._dim if no_row_in_plot <= 1 else col_sz or no_col_in_plot
         CMAP = CMAP or plt.cm.get_cmap('RdYlBu_r')
         msz0, msz1 = som.codebook.mapsize
 
         if what == 'codebook':
             h = .1
             w = .1
-            fig = plt.figure(figsize=(no_col_in_plot*2.5*(1+w), no_row_in_plot*2.5*(1+h)))
-            DD = pd.Series(data=codebook.flatten()).describe(percentiles=[.03, .05, .1, .25, .3, .4, .5, .6, .7, .8, .9, .95, .97])
-            norm = matplotlib.colors.Normalize(vmin=DD.ix['3%'], vmax=DD.ix['97%'], clip=False)
+            self.width = no_col_in_plot*2.5*(1+w)
+            self.height = no_row_in_plot*2.5*(1+h)
+            self.prepare()
 
             while axisNum < som._dim:
                 axisNum += 1
 
-                ax = fig.add_subplot(no_row_in_plot, no_col_in_plot, axisNum)
+                ax = self._fig.add_subplot(no_row_in_plot, no_col_in_plot, axisNum)
                 ind = int(indtoshow[axisNum-1])
                 mp = codebook[:, ind].reshape(msz0, msz1)
+                plt.imshow(mp[::-1], norm=None, cmap=CMAP)
 
-                if grid == 'yes':
-                   pl = plt.pcolor(mp[::-1], cmap=CMAP)
-
-                elif grid == 'no':
-                    plt.imshow(mp[::-1], norm=None, cmap=CMAP)
-                    plt.axis('off')
-
-                if text == 'yes':
-                    plt.title(som.component_names[0][ind])
-                    font = {'size': self.text_size}
-                    plt.rc('font', **font)
-
-                plt.axis([0, msz0, 0, msz1])
-                ax.set_yticklabels([])
-                ax.set_xticklabels([])
-                ax.xaxis.set_ticks([i for i in range(0,msz1)])
-                ax.yaxis.set_ticks([i for i in range(0,msz0)])
-                ax.xaxis.set_ticklabels([])
-                ax.yaxis.set_ticklabels([])
-                ax.grid(True, linestyle='-', linewidth=0.5,color='k')
-
-            plt.subplots_adjust(hspace=h, wspace=w)
+                self._set_axis(ax, msz0, msz1)
 
         if what == 'cluster':
-            if hasattr(som, 'cluster_labels'):
-                codebook = som.cluster_labels
-
-            else:
-                #print 'clustering based on default parameters...'
-                codebook = som.cluster()
+            codebook = som.cluster_labels if hasattr(som, 'cluster_labels') else som.cluster()
 
             h = .2
             w = .001
-            fig = plt.figure(figsize=(msz0/2, msz1/2))
+            self.width = msz0/2
+            self.height = msz1/2
+            self.prepare()
 
-            ax = fig.add_subplot(1, 1, 1)
+            ax = self._fig.add_subplot(1, 1, 1)
             mp = codebook[:].reshape(msz0, msz1)
-            if grid == 'yes':
-                plt.imshow(mp[::-1], cmap=CMAP)
+            plt.imshow(mp[::-1], cmap=CMAP)
 
-            elif grid == 'no':
-                plt.imshow(mp[::-1], cmap=CMAP)
-                plt.axis('off')
+            self._set_axis(ax, msz0, msz1)
 
-            if text == 'yes':
-                plt.title('clusters')
-                font = {'size': self.text_size}
-                plt.rc('font', **font)
-
-            plt.axis([0, msz0, 0, msz1])
-            ax.set_yticklabels([])
-            ax.set_xticklabels([])
-            ax.xaxis.set_ticks([i for i in range(0, msz1)])
-            ax.yaxis.set_ticks([i for i in range(0, msz0)])
-            ax.xaxis.set_ticklabels([])
-            ax.yaxis.set_ticklabels([])
-            ax.grid(True, linestyle='-', linewidth=0.5, color='k')
-            plt.subplots_adjust(hspace=h, wspace=w)
+        plt.subplots_adjust(hspace=h, wspace=w)
 
 
 class View1D(MapView):
 
     def show(self, som, what='codebook', which_dim='all'):
-        indtoshow, no_row_in_plot, no_col_in_plot, axisNum = self._setup_figure(som, which_dim)
+        self.width, self.height, indtoshow, no_row_in_plot, no_col_in_plot, axisNum = self._calculate_figure_params(som, which_dim)
+        self.prepare()
+
         codebook = som.codebook.matrix
 
         while axisNum < som._dim:
@@ -167,8 +136,5 @@ class View1D(MapView):
             ind = int(indtoshow[axisNum-1])
             mp = codebook[:, ind]
             plt.plot(mp, '-k', linewidth=0.8)
-            plt.title(som.component_names[0][ind])
-            font = {'size': self.text_size*self.height/no_col_in_plot}
-            plt.rc('font', **font)
 
         plt.show()
