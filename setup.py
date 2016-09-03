fafdbb1411630c459ffc6cd079ed433aa157092d
Vahid Moosavi <sevamoo@gmail.com>
2016-09-03 22:41:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name="SOMPY",
     version="1.0",
     description="Self Organizing Maps Package",
-    author="",
+    author="Vahid Moosavi and Sebastian Packmann",
     packages=find_packages(),
     install_requires=['numpy >= 1.7', 'scipy >= 0.9',
                       'scikit-learn >= 0.16', 'numexpr >= 2.5']
